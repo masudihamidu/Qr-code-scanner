cc44bd9808d7c6e9ef525e69701a974d9e7a2fd8
masoud hamidu <masudihmfaume@gmail.com>
2023-09-25 18:56:19 +0300

    creating function that used for qr controller

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# qr_scanner
+# scanner_qr
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:qr_scanner/screens/Home.dart';
+
+import 'Home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,17 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Home(),
+      home: const Home(),
     );
   }
 }
-
 
